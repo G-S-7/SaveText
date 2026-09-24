c6925b59fe511cc33ed88adba55a0b2936d2520c
G-S-7 <sgianchandani@outlook.com>
2026-09-24 10:46:11 +0530

    Update cookie settings for secure authentication and enhance CORS configuration

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -17,8 +17,8 @@ export const Signup = async (req, res, next) => {
     // });
     res.cookie("token", token, {
   httpOnly: true,
-  sameSite: "lax",   // or "none" if cross-site
-  secure: false,     // true if HTTPS
+  sameSite: "none",   // or "none" if cross-site
+  secure: true,     // true if HTTPS
 });
     res
       .status(201)
@@ -44,10 +44,15 @@ export const Login = async (req, res, next) => {
       return res.json({message:'Incorrect password or email' }) 
     }
      const token = createSecretToken(user._id);
+    //  res.cookie("token", token, {
+    //    withCredentials: true,
+    //    httpOnly: false,
+    //  });
      res.cookie("token", token, {
-       withCredentials: true,
-       httpOnly: false,
-     });
+  httpOnly: true,
+  sameSite: "none",   // or "none" if cross-site
+  secure: true,     // true if HTTPS
+});
      res.status(201).json({ message: "User logged in successfully", success: true, token:token });
      next()
   } catch (error) {
